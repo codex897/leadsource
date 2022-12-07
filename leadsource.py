028af164ba61d390b97e3eafe50d034d245d6b4d
Alex Yoseph <ayose897@mtroyal.ca>
2022-12-06 19:35:17 -0700

    added 2 files

---
+++ leadsource.py
@@ -0,0 +1,21 @@
+
+
+def take( ) -> str:
+    source = LeadSource()
+    phone_number = source.take()
+
+    pass
+
+def add(phone: str) -> str:
+    pool = LeadPool()
+    pool.add("403-867-5309")
+    pass
+
+def is_full(: ) -> bool:
+
+
+def write_pool()
+
+def main():
+
+    main()
